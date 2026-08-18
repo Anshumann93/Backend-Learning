2e919c98584395cc80d69abb025fc00f607f1e7c
Anshumann93 <anshumanatitirmare.cse23f@kdkce.edu.in>
2026-08-18 23:51:42 +0530

    Change password added

--- Src/controller/user.controller.js
+++ Src/controller/user.controller.js
@@ -4,7 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/Cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import mongoose from "mongoose";
-import { verify } from "jsonwebtoken";
+// import { verify } from "jsonwebtoken";
 import {VerifyJwt} from "../middleware/auth.middleware.js"
     const generateAccessTokensAndRefrshToken= async(userId)=>{
        try{
@@ -218,7 +218,25 @@ const userLogin = asyncHandler(async(req,res)=>{
        
     })
 
-    
+    const changePassword = asyncHandler(async(req,res)=>{
+
+        const { oldPassword , newPassword } = req.body
+
+        const user = await User.findOne(user._id) 
+
+        const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+        
+        if(!isPasswordCorrect){
+            throw new ApiError(400, "Invalid old password")
+        }
+
+        user.password = newPassword
+        await user.save({validateBeforeSave:false})
+
+        return res
+        .status(200)
+        .json(new ApiResponse(200,{},"Password Changed Successfully"))
+    })
 
 export { userRegister,
          userLogin,
